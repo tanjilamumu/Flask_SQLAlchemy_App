b9190b1e25e19a89d3eefa7b943b55a0eba2d157
tanjilamumu <tanjilamumu@ymail.com>
2026-01-16 01:00:24 -0500

    add comments to code

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def home():
 
     return render_template('index.html', tasks=tasks, BUCKET_NAME=BUCKET_NAME)
 
-
+#add task
 @app.route('/add', methods=['POST'])
 def add_task():
     task = request.form.get('task')
@@ -119,7 +119,7 @@ def add_task():
 
 
 
-
+#delete task
 @app.route('/delete/<int:task_id>')
 def delete_task(task_id):
     if 'user' not in session:
@@ -131,6 +131,7 @@ def delete_task(task_id):
         db.session.commit()
     return redirect('/')
 
+#edit task
 @app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
 def edit_task(task_id):
     if 'user' not in session:
@@ -158,6 +159,5 @@ def toggle_task(task_id):
     return redirect('/')
 
 
-# ------------------ RUN ------------------
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
